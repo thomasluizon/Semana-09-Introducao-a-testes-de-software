732df5e7f7d021c49b62658894277f83740eed6c
thomasluizon <thomaslrgregorio@gmail.com>
2024-12-03 21:12:12 -0300

    fix: highest grade logic and feat: interval test

--- aluno.py
+++ aluno.py
@@ -1,8 +1,10 @@
 class Aluno:
-  def __init__(self, nome, sobrenome, nota):
-    self.nome = nome;
-    self.sobrenome = sobrenome;
-    self.nota = nota;
+    def __init__(self, nome, sobrenome, nota):
+        self.nome = nome
+        self.sobrenome = sobrenome
+        self.nota = nota
 
-  def mostrarAluno(self):
-    return 'Aluno: ' + self.nome + ' ' + self.sobrenome + ' - Nota: ' + str(self.nota);
+    def mostrarAluno(self):
+        return (
+            "Aluno: " + self.nome + " " + self.sobrenome + " - Nota: " + str(self.nota)
+        )
--- testes.py
+++ testes.py
@@ -1,35 +1,44 @@
-import unittest;
-import aluno as a;
-import turma as t;
+import unittest
+import aluno as a
+import turma as t
+
 
 class turmaTest(unittest.TestCase):
+    def setUp(self):
+        print("Teste", self._testMethodName, "sendo executado...")
+        self.alunos = []
 
-  def setUp(self):
-    print('Teste', self._testMethodName, 'sendo executado...');
-    self.alunos = [];
-    
-    self.alunos.append(a.Aluno('Fabio', 'Teixeira', 10));
-    self.alunos.append(a.Aluno('Fabiano', 'Teixeira', 8));
-    self.alunos.append(a.Aluno('Melissa', 'Teixeira', 6));    
-    self.alunos.append(a.Aluno('Angela', 'Teixeira', 7));       
-    
-    self.turmaObject = t.Turma();
-    self.turmaObject.cadastrarAlunos(self.alunos);
-  
-  def tearDown(self):
-    print('Teste', self._testMethodName, 'finalizado.'); 
-  
-  def testMaior(self):      
-    self.assertEqual(10, self.turmaObject.maiorNota.nota, 'Erro ao encontrar maior nota');
+        self.alunos.append(a.Aluno("Fabio", "Teixeira", 10))
+        self.alunos.append(a.Aluno("Fabiano", "Teixeira", 8))
+        self.alunos.append(a.Aluno("Melissa", "Teixeira", 6))
+        self.alunos.append(a.Aluno("Angela", "Teixeira", 7))
 
-  def testMenor(self):    
-    self.assertEqual(6, self.turmaObject.menorNota.nota, 'Erro ao encontrar menor nota');
+        self.turmaObject = t.Turma()
+        self.turmaObject.cadastrarAlunos(self.alunos)
 
-  def testIntervalo(self):
-    print('Testar se o intervalo de notas está correto.')
-    #Escreva o seu código aqui
-    #Testar se o intervalo de notas está entre 0 e 10.
+    def tearDown(self):
+        print("Teste", self._testMethodName, "finalizado.")
+
+    def testMaior(self):
+        self.assertEqual(
+            10, self.turmaObject.maiorNota.nota, "Erro ao encontrar maior nota"
+        )
+
+    def testMenor(self):
+        self.assertEqual(
+            6, self.turmaObject.menorNota.nota, "Erro ao encontrar menor nota"
+        )
+
+    def testIntervalo(self):
+        print("Testar se o intervalo de notas está correto.")
+        for aluno in self.turmaObject.turma:
+            self.assertGreaterEqual(
+                aluno.nota, 0, f"Nota menor que 0 encontrada para {aluno.nome}."
+            )
+            self.assertLessEqual(
+                aluno.nota, 10, f"Nota maior que 10 encontrada para {aluno.nome}."
+            )
 
 
 if __name__ == "__main__":
-  unittest.main()
+    unittest.main()
